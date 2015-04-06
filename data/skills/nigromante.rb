bcfb789f9a6415267681a257c71acdaeb1a57282
Borja Martín Fernández <bmartinf@axpe.com>
2015-04-06 18:48:08 +0200

    Fixes in habs

--- data/skills/nigromante.rb
+++ data/skills/nigromante.rb
@@ -4,18 +4,25 @@ def nigromantes
     {
       :id           => 0,
       :name         => "levantar muertos",
-      :description  => "sin definir",
-      :type         => "única"
+      :description  => "2 PM: Invoca a un esquelo para que luche bajo tu mando.",
+      :type         => "Activa",
+      :nivel        => 1
     },
     {
       :id           => 1,
       :name         => "explotar cadáver",
-      :description  => "sin definir",
-      :type         => "pasiva"
+      :description  => "1PM: Explota un cadáver en la sala o pasillo, causando 1DC psiónicos a su alrededor.",
+      :type         => "Reactiva",
+      :nivel        => 1
+    },
+    {
+      :id           => 2,
+      :name         => "Cenizas fúnebres",
+      :description  => "Añade 2 hechizos adicionales de sombras al iniciar el reto.",
+      :type         => "Pasiva",
+      :nivel        => 1
     }
   ]
 end
 
-def nigromante(id)
-  nigromantes[id]
-end
+def nigromante id ; nigromantes[id] end
