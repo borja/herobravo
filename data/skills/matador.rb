bbd8fd2cdbe9c5fc63f3a3ac660e98c702f25713
Borja Martín Fernández <bmartinf@axpe.com>
2015-06-15 13:43:53 +0200

    Fix matador skill imgs

--- data/skills/matador.rb
+++ data/skills/matador.rb
@@ -3,33 +3,33 @@ def matadors
   [
     {
       :id           => 0,
-      :name         => "ataque combinado",
+      :name         => "Ataque combinado",
       :description  => "el matador usa sus dos armas simultáneamente en un ataque conjunto.",
-      :type         => "única"
+      :type         => "Única"
     },
     {
       :id           => 1,
       :name         => "Dos armas",
       :description  => "el matador puede llevar un hacha de mano en la mano torpe",
-      :type         => "pasiva"
+      :type         => "Pasiva"
     },
     {
       :id           => 2,
       :name         => "Arrojar",
       :description  => "el matador puede arrojar un arma como acción gratuita",
-      :type         => "pasiva"
+      :type         => "Pasiva"
     },    
     {
       :id           => 3,
       :name         => "Sangre por sangre",
       :description  => "La fuerza de los golpes del matador es igual a su inferioridad numérica en meleé.",
-      :type         => "pasiva"
+      :type         => "Pasiva"
     },
     {
       :id           => 4,
       :name         => "Tenaza del enano",
       :description  => "+1 Potencia al golpear por el flanco a enemigos con menos movimiento.",
-      :type         => "pasiva"
+      :type         => "Pasiva"
     }
   ]
 end
