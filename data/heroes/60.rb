7ea6409291ff509dca9f632f7dd799d2b9c90c63
Borja Martín Fernández <bmartinf@axpe.com>
2015-08-05 22:07:31 +0200

    Fix Killarney family

--- data/heroes/60.rb
+++ data/heroes/60.rb
@@ -1,19 +1,20 @@
 def h60
   {
-    :id        => 60,
-    :name      => "Anníbal Killarney",
-    :personaje => "hoplita",
-    :jugador   => "Iris",
-    :status    => "reserva",
-    :nivel     => 2,
-    :cuerpo    => 8,
-    :mente     => 2,
-    :mov       => 8,
-    :profesion => Profesion.new({:id => 3, :aprendiz => [0,1]}),
-    :armadura  => Armadura.new({:id => 2}),
-    :armas     => [
+    :id           => 60,
+    :name         => "Anníbal Killarney",
+    :personaje    => "hoplita",
+    :jugador      => "Iris",
+    :status       => "reserva",
+    :progenitores => [48,"Amílcar"],
+    :nivel        => 2,
+    :cuerpo       => 8,
+    :mente        => 2,
+    :mov          => 8,
+    :skills       => [0,2],
+    :profesion    => Profesion.new({:id => 3, :aprendiz => [0,1]}),
+    :armadura     => Armadura.new({:id => 2}),
+    :armas        => [
       Arma.new({:id => 1}),
       Arma.new({:id => 7})],
-    :skills    => [0,2],
   }
 end
