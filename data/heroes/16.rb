6d8f18051d030758aa10cb0ca35edcf7ab012a3a
Borja Martín Fernández <bmartinf@axpe.com>
2015-09-02 00:48:29 +0200

    Trade with iris and chorni

--- data/heroes/16.rb
+++ data/heroes/16.rb
@@ -13,14 +13,21 @@ def h16
     :mov          => 7,
     :hechizos     => [1,2,3,4,5,6,7,8,9,10,11,12,29,33],
     :skills       => [0,1,2,5],
-    :armas        => [Arma.new({:id => 3,  :enchants => [54]}), 
-                     Arma.new({:id => 4,  :enchants => [13,14,15] })],
-    :armadura     => Armadura.new({:id => 1,   :gemas    => [4,4,4]}),
-    :proteccions  => [Proteccion.new({:id => 1,  :enchants => [55]}), 
-                     Proteccion.new({:id => 3,  :gemas    => [26], :ranuras => 1}), 
-                     Proteccion.new({:id => 8,  :gemas    => [29]})],
-    :miscelaneas  => [Miscelanea.new({:id => 1,  :gemas    => [19],     :enchants => [56] }), 
-                     Miscelanea.new({:id => 2,  :gemas    => [28]}),
-                     Miscelanea.new({:id => 6,  :enchants => [57]})],
-    }
+    :armas        => [
+      Arma.new({:id => 3,  :enchants => [54]}), 
+      Arma.new({:id => 4,  :enchants => [13,14,15] })
+    ],
+    :armadura     => Armadura.new({:id => 1, :gemas => [4,4,4]}),
+    :proteccions  => [
+      Proteccion.new({:id => 1, :enchants => [55]}), 
+      Proteccion.new({:id => 3, :ranuras => 2}), 
+      Proteccion.new({:id => 8, :gemas => [26] })
+    ],
+    :miscelaneas  => [
+      Miscelanea.new({:id => 1, :gemas => [28], :enchants => [56] }),
+      Miscelanea.new({:id => 1, :gemas => [25] }), 
+      Miscelanea.new({:id => 2, :ranuras => 1 }),
+      Miscelanea.new({:id => 6, :enchants => [57]})
+    ],
+  }
 end
