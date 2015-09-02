0dc94ab22f2e4484385522f41348e41f62ec9661
Borja Martín Fernández <bmartinf@axpe.com>
2015-09-02 23:35:20 +0200

    caminante rework I

--- data/skills/caminante.rb
+++ data/skills/caminante.rb
@@ -4,21 +4,32 @@ def caminantes
     {
       :id           => 0,
       :name         => "golpe sombrío",
-      :description  => "A cambio de 1 PS, realizas una trapera a un enemigo a melé",
-      :type         => "activa"
+      :nivel        => 2,
+      :description  => "1 PS: +1 potencia a un golpe (no-frontal) a melé.",
+      :type         => "Activa"
     },
     {
       :id           => 1,
-      :name         => "sendero oscuro",
-      :description  => "A cambio de 7 de movimiento, ganas 1 PS",
-      :type         => "activa"
+      :name         => "Sendero oscuro",
+      :nivel        => 2,
+      :description  => "Al pasar por una casilla en la que haya muerto un enemigo este turno, obtienes +1 PS.",
+      :type         => "Reactiva"
     },
     {
       :id           => 2,
-      :name         => "recorrer la umbra",
-      :description  => "Acambio de 1 PS, ganas +7 movimiento",
-      :type         => "activa"
-    }
+      :name         => "Recorrer la umbra",
+      :nivel        => 2,
+      :description  => "1 PS: Coloca tu miniatura en una casilla libre adyacente a una puerta de la misma sala o pasillo.",
+      :type         => "Activa"
+    },
+    {
+      :id           => 3,
+      :name         => "Secuestrar sombra",
+      :description  => "1 PS: Secuestra la sombra de su enemigo permitiendo un ataque a distancia. Al matar, se teleporta a su posición.",
+      :type         => "Activa",
+      :nivel        => 2,
+    },
+    
   ]
 end
 
