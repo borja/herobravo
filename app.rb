57efae126ce506b0fbd2fc50886f353e691d1df9
Borja Martín Fernández <bmartinf@axpe.com>
2015-09-08 11:46:54 +0200

    Fix escuelas and refactor routing

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra/base'
-
 class App < Sinatra::Base
   
   get '/' do erb :index end 
@@ -11,19 +10,16 @@ class App < Sinatra::Base
       when 'hero'        then erb :'ficha/ficha'
       when 'city'        then erb :'ciudad/ciudad'        
       when 'habilidades' then preview('disciplinas')
-      when 'aire' || 'agua' || 'fuego' || 'tierra' then preview('hechizos')      
-      else preview(view) # Try same view as URL (with params)            
+      when 'aire','agua','fuego','tierra' then preview('hechizos')      
+      else preview(view) # Try same view as URL (with params) 
     end
   end
   
   # Templates with simple root-viewer  
   get '/:view' do |view|
-    lista_heroes = ["heroes","reservistas","extranjeros","ausentes","licenciados"]
-    case
-      when view == 'gemas'     then erb :'items/gemas/gemas'
-      when view == 'escuelas'  then erb :'magia/escuelas'
-      when view == 'criaturas' then erb :'reglamento/criaturas'
-      when lista_heroes.include?(view) then erb :template, :locals => view('heroes')
+    case view
+      when 'criaturas' then erb :'reglamento/criaturas'
+      when "heroes" || "reservistas" || "extranjeros" || "ausentes" || "licenciados" then preview('heroes')
       else preview(view) # Try same view as URL
     end
   end
