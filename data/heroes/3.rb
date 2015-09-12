96c8ab57c6a0c3c7dbe63be2303e84919c4eff26
Borja Martín Fernández <bmartinf@axpe.com>
2015-09-12 11:37:33 +0200

    Big <section> refactor. step2/2

--- data/heroes/3.rb
+++ data/heroes/3.rb
@@ -5,6 +5,7 @@ def h3
     :name         => "Elysa Demoneye",
     :personaje    => "vengador",
     :jugador      => "Iris",
+    :progenitores => ["Beliel","Elena"],
     :titulo       => "Condesa de Zelmuria",
     :gender       => "female",
     :status       => "reserva",
