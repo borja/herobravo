a2a33360a73311730879ac0385951d1b0660415c
Borja Martín Fernández <bmartinf@axpe.com>
2015-09-13 03:34:33 +0200

    First caminos rework

--- data/caminos.rb
+++ data/caminos.rb
@@ -3,31 +3,74 @@ def caminos
 	  "El Heredero"   => [
 	    ["La carta"      , "Recibes una carta inesperada"],
       ["Los ancestros" , "Debes ayudar al fantasma de un antepasado"],
+      ['El cónclave'   , "Un grupo de monjes revelan una profecía"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"]
+    ],
+	  "El Burgués"    => [
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"]
 	  ],
-	  "El Burgués"    => [],
 	  "El Arconte"    => [
-      ["algo", "algo mas"],
-      ["algo2", "y mas..."]
+      ['La revuelta'   , "Unos ganaderos sentenciados a muerte, se levantan contra el Imperio"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
     ],
 	  "El Justiciero" => [
-      ["algo", "algo mas"],
-      ["algo2", "y mas..."]
+      ['Tormento'      , "El veneno más potente es el ácido de una idea de venganza."],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
     ],
 	  "El Eremita"    => [
-      ["algo", "algo mas"],
-      ["algo2", "y mas..."]
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
     ],
 	  "El Santo"      => [
-      ["algo", "algo mas"],
-      ["algo2", "y mas..."]
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
     ],
     "El Bardo"      => [
-      ["algo", "algo mas"],
-      ["algo2", "y mas..."]
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
     ],
     "El Oscuro"     => [
-      ["La Piedra", "Descubres un pendril perdido y hablas con el señor oscuro al otro lado."],
-      ["Susurros", "..."]
+      ["La piedra"     , "Descubres un pendril perdido y hablas con el señor oscuro al otro lado."],
+      ["Susurros"      , "..."],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
+      ['Bloqueado'     , "Historia por descubrir"],
     ],
   }
 end
