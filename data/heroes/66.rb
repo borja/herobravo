b5418100dc8785e6d76d7a880b1e098553f4506e
Borja Martín Fernández <bmartinf@axpe.com>
2015-09-15 20:24:26 +0200

    Add Cabs heroes. Welcome

---
+++ data/heroes/66.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+def h66
+  { 
+    :id           => 66,
+    :name         => "Jammarcus",
+    :personaje    => "druida",
+    :jugador      => "Gon",
+    :ciudad       => "Kellen",
+    :nivel        => 1,
+    :cuerpo       => 6,
+    :mente        => 4,
+    :mov          => 8,
+    :skills       => [0],
+    :armadura     => Armadura.new({:id => 2}),
+    :armas        => [
+      Arma.new({:id => 1}),
+      Arma.new({:id => 7})
+    ]
+  }
+end
