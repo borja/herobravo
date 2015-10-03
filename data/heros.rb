b266c355cfcbb34f7e918c0b0e28fffd281e85c9
Borja Martín Fernández <bmartinf@axpe.com>
2015-10-03 18:01:47 +0200

    add erica

--- data/heros.rb
+++ data/heros.rb
@@ -1,7 +1,7 @@
 ﻿# encoding: UTF-8
-# Number of heroes = 68, please refactor this.
-68.times { |i| require "./data/heroes/#{i}" }
-def heros      ; [*0..67].map { |i| Hero.new(send("h#{i}")) } end
+# Number of heroes = 69, please refactor this.
+69.times { |i| require "./data/heroes/#{i}" }
+def heros      ; [*0..68].map { |i| Hero.new(send("h#{i}")) } end
 def hero id    ; heros[id] end
 def personajes ; heros.map{ |p| p.personaje }.uniq end
 def clases     ; heros.map{ |p| p.clase     }.uniq end
