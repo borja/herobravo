0b627ad3e2c333bc16e14510ba88ee1943b6f6dd
Borja Martín Fernández <bmartinf@axpe.com>
2015-12-27 23:16:23 +0100

    update cristina

--- data/heroes/14.rb
+++ data/heroes/14.rb
@@ -8,10 +8,10 @@ def h14
    :gender       => "female",
    :pareja       => {:char => 15, :tipo => "pj"},
    :historia     => 1,
-   :nivel        => 6,
+   :nivel        => 8,
    :cuerpo       => 8,
-   :mente        => 4,
-   :mov          => 13,
+   :mente        => 6,
+   :mov          => 17,
    :hechizos     => [6,7,8],
    :armas        => [Arma.new({:id => 2, :enchants => [16]}), 
                      Arma.new({:id => 1, :enchants => [75]})],
@@ -23,9 +23,9 @@ def h14
    :skills       => [0,1,2,4,5,6,8],
    :profesion    => Profesion.new({:id => 2,  :aprendiz => [0,1]}),  
    :pet          => Pet.new({:id  => 19, :name => "Limoncito"}),
-   :pociones     => [6,0],
+   :pociones     => [0],
    :piezas       => [0,4],
    :pergaminos   => [Pergamino.new({:id => 1, :spells   => [11] })],
-   :tesoro       => {:gemas => [1]}
+   :tesoro       => {:gemas => [12]}
   }
 end
