a50dec8aa77ac5e9f7f9f946e944db3ce7bd64da
Borja Martín Fernández <bmartinf@axpe.com>
2016-02-08 18:13:02 +0100

    YMLize views

--- model/templates.rb
+++ model/templates.rb
@@ -3,9 +3,26 @@
 
 def get_center(template)
   col = case
-        when template[:right].nil? && template[:left].nil? then 12
-        when template[:right].nil? || template[:left].nil? then 9
+        when template['right'].nil? && template['left'].nil? then 12
+        when template['right'].nil? || template['left'].nil? then 9
         else 6
         end
   "#{col}u important(collapse)"
+end
+
+def views
+  YAML::load_file(File.join('data/views.yml'))
+end
+
+def view(ruta)
+  views.find { |v| v['ruta'] == ruta }
+end
+
+def preview(ruta)
+  erb :template, locals: view(ruta)
+end
+
+def load_partial(position)
+  partial = position.gsub(':param', params[:param].to_s)
+  erb :"#{partial}"
 end
