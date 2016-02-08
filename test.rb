c0850c01ef3d03b09dab503220b4a99023bc1a53
Borja Martín Fernández <bmartinf@axpe.com>
2016-02-08 16:30:52 +0100

    YMLize all skills, except maestrodearmas

--- test.rb
+++ test.rb
@@ -3,19 +3,20 @@ require 'minitest/autorun'
 require 'rack/test'
 require_relative 'app.rb'
 require './model/model'
- 
+
 include Rack::Test::Methods
- 
-def app ; Sinatra::Application end
 
-describe ".hero" do
+def app
+  Sinatra::Application
+end
 
-  it "should say true" do
+describe '.hero' do
+  it 'should say true' do
     assert hero(2).id == 2
   end
-  it "should say true" do
+  it 'should say true' do
     heros.each do |h|
       File.exist?(h.big_path)
     end
-  end  
+  end
 end
