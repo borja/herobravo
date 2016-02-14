ebe727d29c2a59d0491bba677d27bb832c0d5931
Borja Martín Fernández <bmartinf@axpe.com>
2016-02-14 13:57:55 +0100

    Add heroes count in caminos

--- model/camino.rb
+++ model/camino.rb
@@ -1,3 +1,31 @@
+class Camino < Array
+  
+  def name
+    self.first
+  end
+  
+  def pasos
+    self.last
+  end
+  
+  def conocidos
+    total = []
+    heros.each do |h|
+      if h.camino
+        if h.camino.first == name
+          total << h.id
+        end
+      end
+    end
+    total
+  end
+  
+  def img_path
+    "'../images/caminos/#{name}.png'"
+  end
+
+end
+
 def camino(nombre)
   caminos.find {|c| c.first == nombre}
 end 
