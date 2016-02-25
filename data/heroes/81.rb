47787640561543354226a158062de1a0d89a501d
Borja Martín Fernández <bmartinf@axpe.com>
2016-02-25 20:12:04 +0100

    Change Namhayd & Rhemvorn

---
+++ data/heroes/81.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+def h81
+  {
+    id:           81,
+    name:         'Namhayd',
+    personaje:    'arquero',
+    jugador:      'Alpa',
+    status:       'reserva',
+    nivel:        3,
+    repu:         0,
+    cuerpo:       8,
+    mente:        4,
+    mov:          9,
+    skills:       [1, 2, 3, 6],
+    hechizos:     [6, 7, 8],
+    armas:        [
+      Arma.new(id: 25),
+      Arma.new(id: 14)
+    ],
+    armadura:     Armadura.new(id: 2),
+    miscelaneas:  [
+      Miscelanea.new(id: 21, enchants: [29])
+    ]
+  }
+end
