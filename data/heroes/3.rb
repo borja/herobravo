184f77a84ad3e9b9ec042832c80a89e612f4aa37
Borja Martín Fernández <bmartinf@axpe.com>
2016-03-13 22:58:23 +0100

    Lvl up Bietka

--- data/heroes/3.rb
+++ data/heroes/3.rb
@@ -9,32 +9,37 @@ def h3
     titulo:       'Condesa de Zelmuria',
     gender:       'female',
     status:       'reserva',
-    nivel:        7,
+    nivel:        8,
     repu:         2,
-    cuerpo:       9,
+    cuerpo:       10,
     mente:        7,
     mov:          7,
     skills:       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11],
     hechizos:     [0, 1, 2],
     shadows:      [0, 1, 2, 5],
     materiales:   [0],
-    armas:        [Arma.new(id: 24, enchants: [72, 73, 74])],
+    pociones:     [3],
+    armas:        [Arma.new(id: 24, enchants: [72, 73, 74], ranuras: 2)],
     armadura:     Armadura.new(id: 3),
     proteccions:  [
       Proteccion.new(id: 1, enchants: [20], ranuras: 2),
-      Proteccion.new(id: 5, enchants: [22])
+      Proteccion.new(id: 3),
+      Proteccion.new(id: 4),      
+      Proteccion.new(id: 5, enchants: [22]),
+      Proteccion.new(id: 8)
     ],
     miscelaneas:  [
-      Miscelanea.new(id: 2, enchants: [21], ranuras: 2),
-      Miscelanea.new(id: 4, enchants: [71]),
-      Miscelanea.new(id: 17, enchants: [64])
+      Miscelanea.new(id: 2,  enchants: [21], ranuras: 2),
+      Miscelanea.new(id: 4,  enchants: [71]),
+      Miscelanea.new(id: 17, enchants: [64]),
+      Miscelanea.new(id: 1,  enchants: [104], gemas: [29])
     ],
     abalorios:    [Abalorio.new(id: 7)],
     pet:          Pet.new(id: 20, name: 'Jazzy'),
-    profesion:    Profesion.new(id: 3, aprendiz: [0, 5]),
+    profesion:    Profesion.new(id: 3, aprendiz: [0, 1, 5]),
     tesoro:       {
       runas: [3],
-      gemas: [18, 19, 27, 29, 21, 52, 10]
+      gemas: [6, 10, 18, 21, 25, 27, 31, 52]
     }
   }
 end
