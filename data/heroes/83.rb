6db731f4588b64703ea9facb3884ebbf2b77ffea
Borja Martín Fernández <bmartinf@axpe.com>
2016-03-13 22:03:14 +0100

    Add Straüss: Dani de Haro

---
+++ data/heroes/83.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+def h83
+  {
+    id:         83,
+    name:       'Straüss',
+    personaje:  'matador',
+    jugador:    'Daniel de Haro',
+    ciudad:     'Xanandra',
+    nivel:      2,
+    cuerpo:     7,
+    mente:      3,
+    mov:        7,
+    skills:     [0, 2],
+    armas:      [Arma.new(id: 13),
+                 Arma.new(id: 3)],
+    armadura:   Armadura.new(id: 3),
+    pergaminos: [Pergamino.new(id: 3),
+                 Pergamino.new(id: 1, spells: [8]),
+                 Pergamino.new(id: 1, spells: [8])]
+  }
+end
