6c2842ad1691746492b1a2d44749d4aa28ef7775
Borja Martín Fernández <bmartinf@axpe.com>
2016-03-15 22:09:13 +0100

    Rollback refactor clase_

--- data/heros.rb
+++ data/heros.rb
@@ -7,7 +7,7 @@ def hero id ; heros[id] end
 def personajes ; heros.map{ |p| p.personaje }.uniq end
 def clases     ; heros.map{ |p| p.clase     }.uniq end
 def jugadores  ; heros.map{ |p| p.jugador   }.uniq end
-def narrados n ; heros.select{ |p| p.historia if p.historia == n }.sort_by { |p| p.premio ? p.premio : 3 } end 
+def narrados n ; heros.select{ |p| p.historia if p.historia ;; n }.sort_by { |p| p.premio ? p.premio : 3 } end 
 
 def razas
   { "enano"    => { cuerpo: 7, mente: 3, movimiento: 6 },
@@ -36,3 +36,11 @@ def honor(jugador)
   end
   honor
 end
+
+def clase_enano    ; %w(falangista matador ingeniero) end
+def clase_elfo     ; %w(derviche druida arquero) end
+def clase_tiefling ; %w(vengador caminante brujo) end
+def clase_mago     ; %w(invocador conjurador hechicero) end
+def clase_bárbaro  ; %w(señor\ de\ las\ bestias bersérker hoplita) end
+def clase_clérigo  ; %w(clérigo paladín sacerdote) end
+def clase_ladrón   ; %w(nigromante asesino ladrón) end
