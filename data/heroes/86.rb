2ab35ac148d329adb7a8f07f6e5570b9f7009afe
Borja Martín Fernández <bmartinf@axpe.com>
2016-03-16 16:58:06 +0100

    Add Zehon, rip

---
+++ data/heroes/86.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+def h86
+  {
+    id:        86,
+    name:      'Zehon',
+    personaje: 'hoplita',
+    jugador:   'Daniel de Haro',
+    status:    'retirado',
+    muerto:    true,
+    nivel:     1,
+    cuerpo:    8,
+    mente:     2,
+    mov:       7,
+    pet:       Pet.new(id: 17, name: 'Chip'),
+    armadura:  Armadura.new(id: 2),
+    armas:     [
+      Arma.new(id: 7),
+      Arma.new(id: 2)
+    ]
+  }
+end
