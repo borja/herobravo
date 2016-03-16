8d5ee76f9e6d07b6d1ce277b2e3515e054a461f9
Borja Martín Fernández <bmartinf@axpe.com>
2016-03-16 15:09:03 +0100

    Add Grüber (Aitor)

---
+++ data/heroes/85.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+def h85
+  {
+    id:         85,
+    name:       'Grüber',
+    personaje:  'falangista',
+    jugador:    'Aitor',
+    ciudad:     'Xanandra',
+    nivel:      2,
+    cuerpo:     8,
+    mente:      3,
+    mov:        6,
+    skills:     [3, 4],
+    armadura:   Armadura.new(id: 2),
+    armas:      [
+      Arma.new(id: 7),
+      Arma.new(id: 11)
+    ],
+    miscelaneas:[
+      Miscelanea.new(id: 22, enchants: [10])
+    ],
+    pergaminos: [
+      Pergamino.new(id: 3),
+      Pergamino.new(id: 1, spells: [8]),
+      Pergamino.new(id: 1, spells: [8])
+    ]
+  }
+end
