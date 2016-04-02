44bf9f545d188c634fa11d5ef02cc4b126b38444
Borja Martín Fernández <bmartinf@axpe.com>
2016-04-03 00:25:22 +0200

    Engarces en Bietka

--- data/heroes/3.rb
+++ data/heroes/3.rb
@@ -19,7 +19,7 @@ def h3
     shadows:      [0, 1, 2, 5],
     materiales:   [0, 0],
     pociones:     [3],
-    armas:        [Arma.new(id: 24, enchants: [72, 73, 74], ranuras: 2)],
+    armas:        [Arma.new(id: 24, enchants: [72, 73, 74], gemas: [40], joyas: [9])],
     armadura:     Armadura.new(id: 3),
     proteccions:  [
       Proteccion.new(id: 1, enchants: [20], ranuras: 2),
