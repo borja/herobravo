9223bfcfcb231d102344e7a68a763335420ca671
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-11 22:57:57 +0200

    Fix rubocop offenses

--- model/magias/magias.rb
+++ model/magias/magias.rb
@@ -1,9 +1,23 @@
 # YAML DB Loader
-def elficas   ; load_yaml('magia/elficas').map   { |s|    Elfica.new(s) } end
-def plegarias ; load_yaml('magia/plegarias').map { |s|  Plegaria.new(s) } end
-def sangres   ; load_yaml('magia/sangres').map   { |s|    Sangre.new(s) } end
-def sombras   ; load_yaml('magia/sombras').map   { |s|    Sombra.new(s) } end
-def spells    ; load_yaml('magia/spells').map    { |s| Elemental.new(s) } end
+def elficas
+  load_yaml('magia/elficas').map   { |s| Elfica.new(s) }
+end
+
+def plegarias
+  load_yaml('magia/plegarias').map { |s| Plegaria.new(s) }
+end
+
+def sangres
+  load_yaml('magia/sangres').map   { |s|    Sangre.new(s) }
+end
+
+def sombras
+  load_yaml('magia/sombras').map   { |s|    Sombra.new(s) }
+end
+
+def spells
+  load_yaml('magia/spells').map    { |s| Elemental.new(s) }
+end
 
 # TODO: tune up this!
 # All find ID methods
