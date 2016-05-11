c44951b7bac4f2c721f59794d7a9943ec6c197dc
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-11 22:39:51 +0200

    Modularize magias model

---
+++ model/magias/elfica.rb
@@ -0,0 +1,11 @@
+# Todos los hechizos silvanos
+# TODO: Nuevos hechizos de tipo elfico
+class Elfica < Magia
+  def nivel
+    1 # Pronto hay que aumentar esto
+  end
+
+  def elemento
+    'elfico'
+  end
+end
