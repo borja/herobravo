4613243bde9fbdd09c5e3324bdf03892d9c6e536
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-11 22:57:57 +0200

    Fix rubocop offenses

--- model/personaje/camino.rb
+++ model/personaje/camino.rb
@@ -1,29 +1,34 @@
+# Los caminos de un heroe.
 class Camino < Array
-  
-  def name  ; self.first end
-  def pasos ; self.last  end
-  
+  def name
+    first
+  end
+
+  def pasos
+    last
+  end
+
+  # Es posible que este method sea muy pesado
   def conocidos
     total = []
     heros.each do |h|
       if h.camino
-        if h.camino.first == name
-          total << h.id
-        end
+        total << h.id if h.camino.first == name
       end
     end
     total
   end
-  
+
   def img_path
     "'../images/caminos/#{name}.png'"
   end
-
 end
 
 # YAML DB Loader
-def caminos ; load_yaml('personaje/caminos') end
+def caminos
+  load_yaml('personaje/caminos')
+end
 
 def camino(nombre)
-  caminos.find {|c| c.first == nombre}
+  caminos.find { |c| c.first == nombre }
 end
