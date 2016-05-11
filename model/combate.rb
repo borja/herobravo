4d75faec5f260d2f8c61c332bc968b21779fdf74
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-11 22:28:34 +0200

    Fix rubocop offenses in combate

--- model/combate.rb
+++ model/combate.rb
@@ -1,7 +1,20 @@
 # YAML DB Loader
-def estados      ; load_yaml('combate/estados')      end
-def posiciones   ; load_yaml('combate/posiciones')   end
-def resistencias ; load_yaml('combate/resistencias') end
+def estados
+  load_yaml('combate/estados')
+end
 
-def estado   id ;    estados[id] end
-def posicion id ; posiciones[id] end
+def posiciones
+  load_yaml('combate/posiciones')
+end
+
+def resistencias
+  load_yaml('combate/resistencias')
+end
+
+def estado(id)
+  estados[id]
+end
+
+def posicion(id)
+  posiciones[id]
+end
