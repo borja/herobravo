2ab375f8e531a04b147ad3702d55a0247a292191
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-16 23:02:18 +0200

    Document char test lib

--- lib/test.rb
+++ lib/test.rb
@@ -1,22 +1,24 @@
 # encoding: UTF-8
 
+# Dictionary for the character test (14 questions)
 def preguntas
   load_yaml('personaje/test')
 end
 
 # Recoge los valores de las respuestas y los agrupa
-def collect_values(hashes)
+def collect_values(hashes) # An array of hashes should be provided
   {}.tap { |r| hashes.each { |h| h.each { |k, v| (r[k] ||= []) << v } } }
 end
 
-def respuestas(codigo)
-  if codigo.to_s.length == preguntas.count
+# After question num 14, this will turn on
+def respuestas(codigo) # Code from URL
+  if codigo.to_s.length == preguntas.count # Check size (14)
     preguntas.map do |p|
       r = codigo[p['id']].to_i # respuesta segun el codigo
       p['respuestas'][r].last  # valores de puntuacion segun codigo
     end                        # devuelve {"enano"=>2, "elfo"=>1} 14 veces
   else
-    redirect :error
+    redirect :error # If not 14, then raise error
   end
 end
 
