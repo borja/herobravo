b57658ee44b0cf21aba11bf9d3d270b1b507742b
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-16 21:38:31 +0200

    First step: division by indexes and dictionaries

--- model/items/items.rb
+++ model/items/items.rb
@@ -23,76 +23,7 @@ def runas
   load_yaml('items/engarces/runas').map { |g| Runa.new(g) }
 end
 
-# ID SEARCHING TODO: Myabe this must be some different file.
-# -----------------------------------------------------------
-
-# Items comunes, TODO: comunes como tesoros
-def arma(id)
-  armas[id]
-end
-
-def miscelanea(id)
-  miscelaneas[id]
-end
-
-def proteccion(id)
-  proteccions[id]
-end
-
-def armadura(id)
-  armaduras[id]
-end
-
-def abalorio(id)
-  abalorios[id]
-end
-
-def enchant(id)
-  enchants[id]
-end
-
-def pieza(id)
-  piezas[id]
-end
-
-def pocion(id)
-  pociones[id]
-end
-
-def pergamino(id)
-  pergaminos[id]
-end
-
-def material(id)
-  materiales[id]
-end
-
 # Libro malvado del Tarot
 def tarot
   load_yaml('items/tarot')
-end
-
-# Tesoros, TODO: comunes como tesoros
-def gema(id)
-  gemas.find { |g| g.id == id }
-end
-
-def joya(id)
-  joyas.find { |j| j.id == id }
-end
-
-def runa(id)
-  runas.find { |r| r.id == id }
-end
-
-def gema_ranuras
-  gemas.map { |g| g.fits.keys }.flatten.uniq
-end
-
-def runa_ranuras
-  runas.map { |r| r.fits.keys }.flatten.uniq
-end
-
-def joya_ranuras
-  joyas.map { |j| j.fits.keys }.flatten.uniq
 end
