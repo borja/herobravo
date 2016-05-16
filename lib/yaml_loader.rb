7fddadd0810cfb929ef1dab9bb77cf904b72903b
Borja Martín Fernández <bmartinf@axpe.com>
2016-05-16 23:03:58 +0200

    Document YAML method in lib

--- lib/yaml_loader.rb
+++ lib/yaml_loader.rb
@@ -1,6 +1,7 @@
 # YAML Loader
 require 'yaml'
 
-def load_yaml(path)
+# Main fake DB loading method
+def load_yaml(path) # Path must be in /data
   YAML.load_file(File.join(__dir__, "../data/#{path}.yml"))
-end
+end # YAML keys will be allways strings (never symbols)
