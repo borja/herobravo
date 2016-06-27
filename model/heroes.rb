a93fa8b92afc365b65663710c01216c10bf1b808
Borja Martín Fernández <bmartinf@axpe.com>
2016-06-27 12:54:29 +0200

    Refactor heroes complexity

--- model/heroes.rb
+++ model/heroes.rb
@@ -1,10 +1,7 @@
 # encoding: UTF-8
 
-def total_heros
-  Dir['./data/heroes/*'].length
-end
-
 def heros
+  total_heros = Dir['./data/heroes/*'].length
   [*0..(total_heros - 1)].map do |i|
     h = load_yaml("/heroes/#{i}")
     h['nivel'] = 1 unless h['nivel']
@@ -22,10 +19,6 @@ def personajes
   heros.map(&:personaje).uniq
 end
 
-def clases
-  heros.map(&:clase).uniq
-end
-
 def jugadores
   heros.map(&:jugador).uniq
 end
@@ -35,34 +28,7 @@ def narrados(n)
   narrados.sort_by { |p| p.premio ? p.premio : 3 }
 end
 
-def razas
-  {
-    'enano'    => { cuerpo: 7, mente: 3, movimiento: 6 },
-    'elfo'     => { cuerpo: 6, mente: 4, movimiento: 8 },
-    'humano'   => { cuerpo: 5, mente: 5, movimiento: 7 },
-    'tiefling' => { cuerpo: 6, mente: 4, movimiento: 7 }
-  }
-end
-
-def h_clases
-  {
-    enano:    %w(falangista matador ingeniero),
-    elfo:     %w(derviche druida arquero),
-    tiefling: %w(vengador caminante brujo),
-    mago:     %w(invocador conjurador hechicero),
-    bárbaro:  %w(señor\ de\ las\ bestias bersérker hoplita),
-    clérigo:  %w(clérigo paladín sacerdote),
-    ladrón:   %w(nigromante asesino ladrón)
-  }
-end
-
 def nomalize_pj(pj)
   normalization = { '%C3%A9' => 'é', '%C3%AD' => 'í', '%C3%B3' => 'ó' }
   pj.gsub(/%C3%A9|%C3%AD|%C3%B3/) { |match| normalization[match] }
-end
-
-def leyes
-  [{ nombre: 'hechizos', method: 'spell'  },
-   { nombre: 'shadows',  method: 'sombra' },
-   { nombre: 'blood',    method: 'sangre' }]
 end
