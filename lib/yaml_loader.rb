a0e54b5be369af4ca69e90deb043e8ccd7e7ecde
Borja Martín Fernández <bmartinf@axpe.com>
2016-06-27 21:39:24 +0200

    Better documentation con yaml loader

--- lib/yaml_loader.rb
+++ lib/yaml_loader.rb
@@ -3,5 +3,8 @@ require 'yaml'
 
 # Main fake DB loading method
 def load_yaml(path) # Path must be in /data
+  # path must be a yaml file path
   YAML.load_file(File.join(__dir__, "../data/#{path}.yml"))
-end # YAML keys will be allways strings (never symbols)
+  # YAML keys will be allways strings (never symbols)
+  # Returns hash of values (strings)
+end
