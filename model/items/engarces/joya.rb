418d2ffc7262abc0b48d0f3880fa927fc0558a77
Borja Martín Fernández <bmartinf@axpe.com>
2016-07-02 17:18:30 +0200

    Fake non complete item changes

--- model/items/engarces/joya.rb
+++ model/items/engarces/joya.rb
@@ -1,3 +1,6 @@
 # Tipo de engarce de orfebreria
 class Joya < Engarce
+  def precio_venta
+    100 # Like gem lvl 4
+  end
 end
--- model/items/engarces/runa.rb
+++ model/items/engarces/runa.rb
@@ -1,3 +1,6 @@
 # Tipo de engarce de inscripcion
 class Runa < Engarce
+  def precio_venta
+    100 # Will be 250 in future runes.
+  end
 end
