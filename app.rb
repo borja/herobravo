24d830c58310b615ccdb68f3cb1fdedff11aa8c7
Borja Martín Fernández <bmartinf@axpe.com>
2016-11-11 09:00:34 +0100

    Clean obsolete code

--- app.rb
+++ app.rb
@@ -1,16 +1,17 @@
 require 'sinatra/base'
 # Main sinatra App, using param routing.
 class App < Sinatra::Base
-  # GET Routing views
+# GET Routing views
   get('/') { erb :index }
   error    { preview('error') }
-  
+
   # RevengeQuest custom views
   get('/revenge') { erb :'revenge/index' }
 
-  get '/:view/:param' do |view, param| # Templates with double-routing
-    hechizo = %w(aire agua fuego tierra hielo sombra sangre arena).include?(view)
-    hechizo ? preview('hechizos') : preview(view)
+  get '/:view/:param' do |view, _param| # Templates with double-routing
+    hechizo = %w(aire agua fuego tierra hielo sombra sangre arena)
+    es_hechizo = hechizo.include?(view)
+    es_hechizo ? preview('hechizos') : preview(view)
   end
 
   get '/:view' do |view| # Templates with simple root-viewer
@@ -18,8 +19,7 @@ class App < Sinatra::Base
     campeones ? preview('heroes') : preview(view)
   end
 
-  # POST Routing views
+# POST Routing views
   post('/calculador') { preview('calculador') }
   post('/buscar')     { preview('heroes') }
-
 end
