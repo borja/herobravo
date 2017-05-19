2d6d8fe6fe0cfd7bfed6860272d6fb6ceb80ccee
Borja Martín Fernández <borjamartinfernandez@gmail.com>
2017-05-19 19:58:10 +0200

    Rework for plegarias/execraciones vs lvl

--- app.rb
+++ app.rb
@@ -1,30 +1,40 @@
 require 'sinatra/base'
 # Main sinatra App, using param routing.
 class App < Sinatra::Base
-  # GET Routing views
+
+# GET Routing views
+  # Main page 
   get('/') { erb :index }
+
+  # Basic error page (never 404)
   error    { preview('error') }
 
   # RevengeQuest custom views
   get('/revenge') { erb :'revenge/index' }
 
-  get '/:view/:param' do |view, _param| # Templates with double-routing
-    es_element = %w(aire agua fuego tierra).include?(view)
-    es_sagrada = %w(arena hielo sombra sangre).include?(view)
+  # Templates with double-routing
+  get '/:view/:param' do |view, _param|
+    es_element  = %w(aire agua fuego tierra).include?(view)
+    es_sagrada  = %w(arena hielo sombra sangre).include?(view)
+    es_plegaria = %w(plegarias execraciones).include?(view)
     # Custom case for redundant spell lists
     case
-      when es_element then preview('hechizos')
-      when es_sagrada then preview('sagradas')
-      else preview(view) # Usual rooting
+      # Magic views
+      when es_element  then preview('hechizos')
+      when es_sagrada  then preview('sagradas')
+      when es_plegaria then preview('plegarias')
+      # Usual rooting
+      else preview(view)
     end
   end
 
-  get '/:view' do |view| # Templates with simple root-viewer
+  # Templates with simple root-viewer
+  get '/:view' do |view|
     campeones = %w(reservistas ausentes licenciados).include?(view)
     campeones ? preview('heroes') : preview(view)
   end
 
-  # POST Routing views
+# POST Routing views
   post('/calculador') { preview('calculador') }
   post('/buscar')     { preview('heroes') }
 end
